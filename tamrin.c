9670e00d942f0c4ca40ad44b0012a01b6d66bd7b
mahan-mahmoodi <m.mahmodi1380@gmail.com>
2020-11-21 15:41:40 +0330

    ye commite dg beonvane commite akhar

--- tamrin.c
+++ tamrin.c
@@ -4,6 +4,7 @@ int main()
 {
 	printf("mikhaym roo git tamrin konim\n");
 	printf("mirim vase commite badi roo develop");
+	printf("commite sevom ro bazam roo develop anjam midim badesh mirim roo master merge mikonim");
 
 
 	return (0);
